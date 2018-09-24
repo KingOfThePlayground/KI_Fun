4b9892a26ffc061d705041d2e053f4c77066cdd9
Leo Versteegen <leo.versteegen@freenet.de>
2018-09-24 19:15:32 +0200

    Klassen erstellt

---
+++ Backend/Army.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KI_Fun.Backend
+{
+    class Army
+    {
+        const double MOVING_PROGRESS_NEEDED = 1d;
+        int _size;
+        Province _current;
+        Direction _movingDirection;
+        double _movingProgress;
+    }
+}
---
+++ Backend/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KI_Fun.Backend
+{
+    class Country
+    {
+        double _money;
+        HashSet<Province> _provinces;
+        HashSet<Army> _armies;
+    }
+}
